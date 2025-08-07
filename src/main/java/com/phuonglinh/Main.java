581de56e0219fe13f13aad73ae37c6cc2a88c8ad
Phuonglinh <phuonglinh206203@gmail.com>
2025-08-08 00:15:52 +0700

    add pom.xml and application

--- src/main/java/com/phuonglinh/Main.java
+++ src/main/java/com/phuonglinh/Main.java
@@ -1,7 +1,24 @@
 package com.phuonglinh;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        SessionFactory sessionFactory = (SessionFactory) context.getBean("sessionFactory");
+        try (Session session = sessionFactory.openSession()) {
+            if (session != null) {
+                System.out.println("Kết nối Hibernate thành công!");
+            } else {
+                System.out.println("Không thể mở session.");
+            }
+        } catch (Exception e) {
+            System.out.println("Lỗi khi kết nối Hibernate:");
+            e.printStackTrace();
+        }
+        ((ClassPathXmlApplicationContext) context).close();
     }
 }
